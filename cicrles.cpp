f46ab833ce4fca41bf3f27c1443515c2544fa6bf
rrrrrrrrrrrrin <0berdieva@mail.ru>
2025-10-21 23:21:42 +0300

    Haven't finished

--- cicrles.cpp
+++ cicrles.cpp
@@ -1,30 +1,74 @@
 ﻿#include <iostream>
+#include <string>
 #include "rgbimage.h"
 
 int main(int argc, char* argv[]) {
 	if (argc < 3) {
 		return 1;
 	}
-	std::ifstream inFile(argv[1]);
+
+	const char* input = argv[1];
+	const char* output = argv[2];
+
+	std::ifstream inFile(input);
 	if (!inFile) {
 		return 2;
 	}
 
 	try 
 	{
-		// Координаты левого верхнего угла изображения (0,0).
-		// Координаты правого нижнего угла изображения (799, 599).
-		// В центре изображения нарисовать квадрат 200 на 200 пикселей белого цвета
+		int width = 0;
+		int height = 0;
 
-		rgbImg img(800, 600);        // Задаем изображение width=800px, height=600px
-		RGB white{ 255, 255, 255 };  // Задаем цвет в модели RGB (по 1 байту на канал).
+		inFile >> width;
+		inFile >> height;
 
-		for (int row = 200; row < 400; ++row)
-			for (int col = 300; col < 500; ++col)
-				img.get(col, row) = white;  // Пиксель в строке row, столбце col красим в белый цвет
+		rgbImg img(width, height);
+		RGB white{ 255, 255, 255 };
 
-		// Сохраняем изображения в BMP-файл
-		img.save("white_square.bmp");
+		int x0 = 0;
+		int y0 = 0;
+		int R = 0;
+
+		int ch;
+		while ( (ch = inFile.get()) != EOF)
+		{
+			// Collect start positions of x,y and a radius
+			inFile >> x0;  
+			inFile >> y0;
+			inFile >> R;
+
+			// Set the origin for the circle, we will offset x and y coordinate by x0, y0 later
+			int x = 0;
+			// The grid increases x to the right and increases y downwards
+			// Place y higher by R on the grid, by decreasing it by R. x's position is not changed; to start drawing a circe
+			int y = -R;
+
+			// Draw a circle
+			while (x < -y)  // The octant is restricted by the line x = -y (consider the grid)
+			{
+				// Take yMid to determine which pixel to color in 
+				// If yMid is inside the circle, color in the pixel at y; if outside, color in the pixel at y+1
+				int yMid = y + 0.5;
+				
+				// Determine if the distance between yMid and the circle's origin is outside the circle 
+				if (x * x + yMid * yMid > R * R) 
+				{
+					++y;  // change y to increase downwards
+				}
+				
+				img.get(x0 + x, y0 + y) = white;
+
+				// Duplicate the algorithm for 1 octant on all 8 octants
+				img.get(x0 - x, y0 - y) = white;
+				img.get(x0 - x, y0 + y) = white;
+				img.get(x0 + x, y0 - y) = white;
+
+				++x;
+			}
+
+			img.save(output);
+		}
 	}
 	catch (std::exception& e) {
 		std::cout << "Error. " << e.what() << std::endl;
