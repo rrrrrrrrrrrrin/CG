4cf3c118d5d39e91398c5f2e02078feb41f7e608
Berdieva Arina <129844334+rrrrrrrrrrrrin@users.noreply.github.com>
2025-10-21 22:06:48 +0300

    Add files via upload

---
+++ cicrles.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include "rgbimage.h"
+
+int main(int argc, char* argv[]) {
+	if (argc < 3) {
+		return 1;
+	}
+	std::ifstream inFile(argv[1]);
+	if (!inFile) {
+		return 2;
+	}
+
+	try 
+	{
+		// Координаты левого верхнего угла изображения (0,0).
+		// Координаты правого нижнего угла изображения (799, 599).
+		// В центре изображения нарисовать квадрат 200 на 200 пикселей белого цвета
+
+		rgbImg img(800, 600);        // Задаем изображение width=800px, height=600px
+		RGB white{ 255, 255, 255 };  // Задаем цвет в модели RGB (по 1 байту на канал).
+
+		for (int row = 200; row < 400; ++row)
+			for (int col = 300; col < 500; ++col)
+				img.get(col, row) = white;  // Пиксель в строке row, столбце col красим в белый цвет
+
+		// Сохраняем изображения в BMP-файл
+		img.save("white_square.bmp");
+	}
+	catch (std::exception& e) {
+		std::cout << "Error. " << e.what() << std::endl;
+	}
+	catch (...) {
+		std::cout << "Unknown error. " << std::endl;
+	}
+	inFile.close();
+	return 0;
+}
---
+++ rgbimage.h
@@ -0,0 +1,191 @@
+#pragma once
+#pragma once
+#include <cstdint>
+#include <stdexcept>
+#include <fstream>
+
+typedef std::uint16_t  WORD;
+typedef std::uint32_t DWORD;
+typedef int LONG;
+typedef unsigned char BYTE;
+
+struct RGB {
+	BYTE Red;
+	BYTE Green;
+	BYTE Blue;
+};
+
+#pragma pack(push,1)
+struct BITMAPFILEHEADER {
+	WORD bfType;
+	DWORD bfSize; // ?????? ?????
+	WORD bfReserved1;
+	WORD bfReserved2;
+	DWORD bfOffBits; // ???????? ?? ???? ?????? (?????? 54 = 16+biSize)
+};
+
+struct BITMAPINFOHEADER {
+	DWORD biSize; // ?????? ????????? ? ??????
+	LONG biWidth; // ?????? ? ????????
+	LONG biHeight;
+	WORD biPlanes; // ?????? ?????? ???? 1
+	WORD biBitCount; // ???-?? ??? ?? ???? 0 | 1 | 4 | 8 | 16 | 24 | 32
+	DWORD biCompression;
+	DWORD biSizeImage;
+	LONG biXPelsPerMeter; // ?????????????? ??????????
+	LONG biYPelsPerMeter; // ???????????? ??????????
+	DWORD biClrUsed; // ???-?? ???????????? ?????? (???? ???????????? ??????? ??????)
+	DWORD biClrImportant; // ???-?? ???????????? ??????
+};
+#pragma pack(pop)
+
+class rgbImg {
+	unsigned int width_;
+	unsigned int height_;
+	RGB** pixels_;
+	void freeMemoryRows(unsigned int rows) {
+		for (unsigned int row = 0; row < rows; ++row) {
+			delete[] pixels_[row];
+			pixels_[row] = nullptr;
+		}
+	}
+	void swap(rgbImg& other) {
+		std::swap(pixels_, other.pixels_);
+		std::swap(width_, other.width_);
+		std::swap(height_, other.height_);
+	}
+	int get_offset(int width) {
+		int offset = 0;
+		if (width % 4)
+			offset = 4 - (3 * width) % 4;
+		return offset;
+	}
+public:
+	rgbImg(unsigned int width = 800, unsigned int height = 600) : width_(width), height_(height), pixels_(nullptr) {
+		RGB zero = { 0, 0, 0 };
+		pixels_ = new RGB * [height_];
+		if (pixels_ == nullptr)
+			throw std::bad_alloc();
+		for (unsigned int row = 0; row < height_; ++row) {
+			pixels_[row] = new RGB[width_];
+			if (pixels_[row] == nullptr) {
+				freeMemoryRows(row);
+				delete[] pixels_;
+				throw std::bad_alloc();
+			}
+			for (unsigned int col = 0; col < width_; ++col) {
+				pixels_[row][col] = zero;
+			}
+		}
+	}
+	rgbImg(const rgbImg& other) :width_(other.width_), height_(other.height_), pixels_(nullptr) {
+		pixels_ = new RGB * [height_];
+		if (pixels_ == nullptr)
+			throw std::bad_alloc();
+		for (unsigned int row = 0; row < height_; ++row) {
+			pixels_[row] = new RGB[width_];
+			if (pixels_[row] == nullptr) {
+				freeMemoryRows(row);
+				delete[] pixels_;
+				throw std::bad_alloc();
+			}
+			for (unsigned int col = 0; col < width_; ++col) {
+				pixels_[row][col] = other.pixels_[row][col];
+			}
+		}
+	}
+	rgbImg(rgbImg&& other) noexcept {
+		width_ = other.width_;
+		height_ = other.height_;
+		pixels_ = other.pixels_;
+		other.pixels_ = nullptr;
+		other.width_ = 0;
+		other.height_ = 0;
+	}
+	rgbImg& operator=(const rgbImg& other) {
+		if (this != &other) {
+			rgbImg tmp(other);
+			swap(tmp);
+		}
+		return *this;
+	}
+	rgbImg& operator=(rgbImg&& other) noexcept {
+		if (this != &other) {
+			swap(other);
+		}
+		return *this;
+	}
+	unsigned int width() const { return width_; }
+	unsigned int height() const { return height_; }
+	RGB get(unsigned int column, unsigned int row) const {
+		if (row >= height_)
+			throw std::out_of_range("Larger than image height");
+		if (column >= width_)
+			throw std::out_of_range("Larger than image width");
+		return pixels_[row][column];
+	}
+	RGB& get(unsigned int column, unsigned int row) {
+		if (row >= height_)
+			throw std::out_of_range("Larger than image height");
+		if (column >= width_)
+			throw std::out_of_range("Larger than image width");
+		return pixels_[row][column];
+	}
+	bool save(const char* filename) {
+		if (pixels_ == nullptr)
+			return false;
+
+		std::ofstream outBMP(filename, std::ios_base::binary);
+		if (!outBMP.is_open()) {
+			throw std::runtime_error("Failed to open output file");
+		}
+
+		const int offset = get_offset(width_);
+		BITMAPFILEHEADER bmfh;
+		char bfType[] = { 'B', 'M' };
+		bmfh.bfType = *((WORD*)bfType);
+		bmfh.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+		bmfh.bfSize = height_ * width_ * 3 + bmfh.bfOffBits;
+		bmfh.bfReserved1 = 0;
+		bmfh.bfReserved2 = 0;
+
+		outBMP.write((char*)&bmfh, sizeof(BITMAPFILEHEADER));
+
+		BITMAPINFOHEADER bmih;
+		bmih.biSize = sizeof(BITMAPINFOHEADER); // ?????? ????????? ? ??????
+		bmih.biWidth = width_;  // ?????? ? ????????
+		bmih.biHeight = height_;
+		bmih.biPlanes = 1;		// ?????? ?????? ???? 1
+		bmih.biBitCount = 24;   // ???-?? ??? ?? ???? 0 | 1 | 4 | 8 | 16 | 24 | 32
+		bmih.biCompression = 0;
+		bmih.biSizeImage = bmfh.bfSize - bmfh.bfOffBits;
+		bmih.biXPelsPerMeter = 1;
+		bmih.biYPelsPerMeter = 1;
+		bmih.biClrUsed = 0;
+		bmih.biClrImportant = 0;
+		outBMP.write((char*)&bmih, sizeof(BITMAPINFOHEADER));
+
+		BYTE* offset_array = new BYTE[offset];
+		for (int i = 0; i < offset; ++i)
+			offset_array[i] = 0;
+
+		for (unsigned int row = height_; row > 0; --row) {
+			for (unsigned int col = 0; col < width_; ++col) {
+				RGB tmp(pixels_[row - 1][col]);
+				std::swap(tmp.Blue, tmp.Red);
+				outBMP.write((char*)&tmp, sizeof(RGB));
+			}
+			outBMP.write((char*)offset_array, offset);
+		}
+		delete[] offset_array;
+		outBMP.close();
+		return true;
+	}
+	~rgbImg() {
+		if (pixels_ != nullptr) {
+			freeMemoryRows(height_);
+			delete[] pixels_;
+			pixels_ = nullptr;
+		}
+	}
+};
