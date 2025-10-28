d5734f03713e6ae3e03cb5d8d9cb5c27ab437811
rrrrrrrrrrrrin <0berdieva@mail.ru>
2025-10-29 00:46:19 +0300

    Implementing Bresenham's circle drawing algorithm

--- CG-circles/cicrles-Midpoint.cpp
+++ CG-circles/cicrles-Midpoint.cpp
@@ -1,150 +1,150 @@
-﻿#include <iostream>
-#include <string>
-#include <cmath>
-#include "rgbimage.h"
-
-/*
-	// Set the origin for the circle, we will offset x and y coordinate by x0, y0 later
-	int x = 0;
-	// The grid increases x to the right and increases y downwards
-	// Place y higher by R on the grid, by decreasing it by R. x's position is not changed; to start drawing a circe
-	int y = -R;
-
-	// Draw a circle
-	while (x < -y)  // The octant is restricted by the line x = -y (consider the grid)
-	{
-		// Take yMid to determine which pixel to color in
-		// If yMid is inside the circle, color in the pixel at y; if outside, color in the pixel at y+1
-		int yMid = y + static_cast<int>(lround(0.5));
-
-		// Determine if the distance between yMid and the circle's origin is outside the circle
-		if (x * x + yMid * yMid > R * R)
-		{
-			++y;  // change y to increase downwards
-		}
-
-		// Color in the pixel; offset x and y coordinate by x0, y0
-		img.get(x + x0, y + y0) = white;  // y = x
-
-		// Duplicate the algorithm for 1 octant on all 8 octants
-		img.get(y + y0, x + x0) = white;  // x = y
-		img.get(x + x0, -y + y0) = white;  // x = -y
-		img.get(y + y0, -x + x0) = white;  // y = -x
-		img.get(-y + y0, -x + x0) = white;  // -y = -x
-		img.get(-x + x0, -y + y0) = white;  // -x = -y
-		img.get(-x + x0, y + y0) = white;  // -x = y
-		img.get(-y + y0, x + x0) = white;  // -y = x
-
-		++x;
-	}
-
-	The general algorithm ro get rid of floats (subsequently division) is to define a decision parameter (error),
-	find its initial value, determine its step value (by finding errorNext - errorCurrent) and changing error in the loop
-	according to the step value under certain conditions (the same algorithm is used to get rid of division in Bresenham's line algorithm)
-
-	1. Let's call our decision parameter (or error) p. In our loop we determine whether y increases by this expression: x * x + yMid * yMid > R * R
-		Then if p = x * x + yMid * yMid - R * R > 0 then ++y. yMId = y + 0.5 => [p = x^2 + (y+0.5)^2 - R^2]
-
-	2. pInitial (initial values of x and yMid are x = 0 and yMid = y + 0.5 = 0.5 - R) = 0 + (0.5-R)^2 - R^2 =
-		= 0.25 - R + R^2 - R^2 = 0.25 - R. We can get rid of the float or multiply every term by the denominator: [pInitial = -R]
-
-	3. Let's go back to p to determine pNext. pNext is p when x+1: pNext (yMid = y + 0.5) = (x+1)^2 + (y + 0.5)^2 - R^2 =
-		= x^2 + 2x + 1 + y^2 + y + 0.25 - R^2 = x^2 + 2x + y^2 + y + 1 - R^2 + 0.25. Let's get rid of the float (could also mupltiply every term by its denominator)
-		[pNext = x^2 + 2x + y^2 + y - R^2 + 1] ( DON'T SUM UP 1 + 0.25 because 1 is derivated from (x+1) in pNext )
-
-	4. pStep = pNext - pCurrent. pCurrent here is p => pStep = x^2 + 2x + y^2 + y - R^2 + 1 - x^2 - y^2 - y - 0.25 + R^2 => [pStep = 2x + 1] (get rid of the float)
-
-	5. Got the value of pStep when y doesn't increase (before recalculation of p in a loop): p = p + pStep. If it does p will change accordingly: (x stays the same)
-		p = p + pIncreased, where pIncreased = pNext - p(y-1) ( p(y-1) = x^2 + (y+0.5-1) - R^2. We subtract 1 from y because y already was incremented
-		before the recalculation of p) = x^2 + 2x + y^2 + y - R^2 + 1 - (x^2 + (y - 0.5)^2 - R^2) =
-		= x^2 + 2x + y^2 + y - R^2 + 1 - x^2 - y^2 + y - 0.25 + R^2 => [pIncreased = 2x + 2y + 1] (get rid of the float)
-
-	6. Finally, if p > 0: the pixel is outside a circle => ++y, x+1 (p = p + pIncreased)
-				if p < 0: the pixel is inside a circle  => y doesn't change, x+1 (p = p + pStep)
-
-*/
-
-int main(int argc, char* argv[]) {
-	if (argc < 3) {
-		return 1;
-	}
-
-	const char* input = argv[1];
-	const char* output = argv[2];
-
-	std::ifstream inFile(input);
-	if (!inFile) {
-		return 2;
-	}
-
-	try 
-	{
-		int width = 0;
-		int height = 0;
-
-		inFile >> width;
-		inFile >> height;
-
-		rgbImg img(width, height);
-		RGB white{ 255, 255, 255 };
-
-		int x0 = 0;
-		int y0 = 0;
-		int R = 0;
-
-		while (inFile.get() != EOF)
-		{
-			// Collect start positions of x,y and a radius
-			inFile >> x0;  
-			inFile >> y0;
-			inFile >> R;
-
-			// Set the origin for the circle, we will offset x and y coordinate by x0, y0 later
-			int x = 0;
-			// The grid increases x to the right and increases y downwards
-			// Place y higher by R on the grid, by decreasing it by R. x's position is not changed; to start drawing a circe
-			int y = -R;
-
-			int p = -R;  // pInitial
-
-			// Draw a circle
-			while (x < -y)  // The octant is restricted by the line x = -y (consider the grid)
-			{
-				// Pixel is outside the circle
-				if (p > 0)
-				{
-					++y;
-					p = p + 2*(x + y) + 1; // p = p + pIncreased;
-				}
-				else  // p < 0: pixel is inside the circle
-				{
-					p = p + 2*x + 1;
-				}
-				
-				// Color in the pixel; offset x and y coordinate by x0, y0
-				img.get(x + x0, y + y0) = white;  // y = x
-
-				// Duplicate the algorithm for 1 octant on all 8 octants
-				img.get(y + y0, x + x0) = white;  // x = y
-				img.get(x + x0, -y + y0) = white;  // x = -y
-				img.get(y + y0, -x + x0) = white;  // y = -x
-				img.get(-y + y0, -x + x0) = white;  // -y = -x
-				img.get(-x + x0, -y + y0) = white;  // -x = -y
-				img.get(-x + x0, y + y0) = white;  // -x = y
-				img.get(-y + y0, x + x0) = white;  // -y = x
-
-				++x;
-			}
-
-			img.save(output);
-		}
-	}
-	catch (std::exception& e) {
-		std::cout << "Error. " << e.what() << std::endl;
-	}
-	catch (...) {
-		std::cout << "Unknown error. " << std::endl;
-	}
-	inFile.close();
-	return 0;
-}
+﻿//#include <iostream>
+//#include <string>
+//#include <cmath>
+//#include "rgbimage.h"
+//
+///*
+//	// Set the origin for the circle, we will offset x and y coordinate by x0, y0 later
+//	int x = 0;
+//	// The grid increases x to the right and increases y downwards
+//	// Place y higher by R on the grid, by decreasing it by R. x's position is not changed; to start drawing a circe
+//	int y = -R;
+//
+//	// Draw a circle
+//	while (x < -y)  // The octant is restricted by the line x = -y (consider the grid)
+//	{
+//		// Take yMid to determine which pixel to color in
+//		// If yMid is inside the circle, color in the pixel at y; if outside, color in the pixel at y+1
+//		int yMid = y + static_cast<int>(lround(0.5));
+//
+//		// Determine if the distance between yMid and the circle's origin is outside the circle
+//		if (x * x + yMid * yMid > R * R)
+//		{
+//			++y;  // change y to increase downwards
+//		}
+//
+//		// Color in the pixel; offset x and y coordinate by x0, y0
+//		img.get(x + x0, y + y0) = white;  // y = x
+//
+//		// Duplicate the algorithm for 1 octant on all 8 octants
+//		img.get(y + y0, x + x0) = white;  // x = y
+//		img.get(x + x0, -y + y0) = white;  // x = -y
+//		img.get(y + y0, -x + x0) = white;  // y = -x
+//		img.get(-y + y0, -x + x0) = white;  // -y = -x
+//		img.get(-x + x0, -y + y0) = white;  // -x = -y
+//		img.get(-x + x0, y + y0) = white;  // -x = y
+//		img.get(-y + y0, x + x0) = white;  // -y = x
+//
+//		++x;
+//	}
+//
+//	The general algorithm ro get rid of floats (subsequently division) is to define a decision parameter (error),
+//	find its initial value, determine its step value (by finding errorNext - errorCurrent) and changing error in the loop
+//	according to the step value under certain conditions (the same algorithm is used to get rid of division in Bresenham's line algorithm)
+//
+//	1. Let's call our decision parameter (or error) p. In our loop we determine whether y increases by this expression: x * x + yMid * yMid > R * R
+//		Then if p = x * x + yMid * yMid - R * R > 0 then ++y. yMId = y + 0.5 => [p = x^2 + (y+0.5)^2 - R^2]
+//
+//	2. pInitial (initial values of x and yMid are x = 0 and yMid = y + 0.5 = 0.5 - R) = 0 + (0.5-R)^2 - R^2 =
+//		= 0.25 - R + R^2 - R^2 = 0.25 - R. We can get rid of the float or multiply every term by the denominator: [pInitial = -R]
+//
+//	3. Let's go back to p to determine pNext. [pNext is p when x+1]: pNext (yMid = y + 0.5) = (x+1)^2 + (y + 0.5)^2 - R^2 =
+//		= x^2 + 2x + 1 + y^2 + y + 0.25 - R^2 = x^2 + 2x + y^2 + y + 1 - R^2 + 0.25. Let's get rid of the float (could also mupltiply every term by its denominator)
+//		[pNext = x^2 + 2x + y^2 + y - R^2 + 1] ( DON'T SUM UP 1 + 0.25 because 1 is derivated from (x+1) in pNext )
+//
+//	4. pStep = pNext - pCurrent. pCurrent here is p => pStep = x^2 + 2x + y^2 + y - R^2 + 1 - x^2 - y^2 - y - 0.25 + R^2 => [pStep = 2x + 1] (get rid of the float)
+//
+//	5. Got the value of pStep when y doesn't increase (before recalculation of p in a loop): p = p + pStep. If it does (++y) p will change accordingly: (x stays the same in p)
+//		p = p + pIncreased, where pIncreased = pNext - p(y-1) ( p(y-1) = x^2 + (y+0.5-1) - R^2. We subtract 1 from y because y already was incremented
+//		before the recalculation of p in the loop; in pNext y is now considered incremented) = x^2 + 2x + y^2 + y - R^2 + 1 - (x^2 + (y - 0.5)^2 - R^2) =
+//		= x^2 + 2x + y^2 + y - R^2 + 1 - x^2 - y^2 + y - 0.25 + R^2 => [pIncreased = 2x + 2y + 1] (get rid of the float)
+//
+//	6. Finally, if p > 0: the pixel is outside a circle => ++y, x+1 (p = p + pIncreased)
+//				if p < 0: the pixel is inside a circle  => y doesn't change, x+1 (p = p + pStep)
+//
+//*/
+//
+//int main(int argc, char* argv[]) {
+//	if (argc < 3) {
+//		return 1;
+//	}
+//
+//	const char* input = argv[1];
+//	const char* output = argv[2];
+//
+//	std::ifstream inFile(input);
+//	if (!inFile) {
+//		return 2;
+//	}
+//
+//	try 
+//	{
+//		int width = 0;
+//		int height = 0;
+//
+//		inFile >> width;
+//		inFile >> height;
+//
+//		rgbImg img(width, height);
+//		RGB white{ 255, 255, 255 };
+//
+//		int x0 = 0;
+//		int y0 = 0;
+//		int R = 0;
+//
+//		while (inFile.get() != EOF)
+//		{
+//			// Collect start positions of x,y and a radius
+//			inFile >> x0;  
+//			inFile >> y0;
+//			inFile >> R;
+//
+//			// Set the origin for the circle, we will offset x and y coordinate by x0, y0 later
+//			int x = 0;
+//			// The grid increases x to the right and increases y downwards
+//			// Place y higher by R on the grid, by decreasing it by R. x's position is not changed; to start drawing a circe
+//			int y = -R;
+//
+//			int p = -R;  // pInitial
+//
+//			// Draw a circle
+//			while (x < -y)  // The octant is restricted by the line x = -y (consider the grid)
+//			{
+//				// Pixel is outside the circle
+//				if (p > 0)
+//				{
+//					++y;
+//					p = p + 2*(x + y) + 1; // p = p + pIncreased;
+//				}
+//				else  // p < 0: pixel is inside the circle
+//				{
+//					p = p + 2*x + 1;
+//				}
+//				
+//				// Color in the pixel; offset x and y coordinate by x0, y0
+//				img.get(x + x0, y + y0) = white;  // y = x
+//
+//				// Duplicate the algorithm for 1 octant on all 8 octants
+//				img.get(y + y0, x + x0) = white;  // x = y
+//				img.get(x + x0, -y + y0) = white;  // x = -y
+//				img.get(y + y0, -x + x0) = white;  // y = -x
+//				img.get(-y + y0, -x + x0) = white;  // -y = -x
+//				img.get(-x + x0, -y + y0) = white;  // -x = -y
+//				img.get(-x + x0, y + y0) = white;  // -x = y
+//				img.get(-y + y0, x + x0) = white;  // -y = x
+//
+//				++x;
+//			}
+//
+//			img.save(output);
+//		}
+//	}
+//	catch (std::exception& e) {
+//		std::cout << "Error. " << e.what() << std::endl;
+//	}
+//	catch (...) {
+//		std::cout << "Unknown error. " << std::endl;
+//	}
+//	inFile.close();
+//	return 0;
+//}
---
+++ CG-circles/circles-Bresenham.cpp
@@ -0,0 +1,205 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+#include "rgbimage.h"
+
+/*
+	Consider the I quarter of the circle. x starts at 0, y starts at R (in our grid)
+	
+	1. We have [3 possible pixels]: pixel is 
+	   [outside the circle] (goes more [horizontal] than the circle outline so we will increment x "to reach it": [++x]),
+	   [inside] (goes more [vertical] so we will decrement y "to reach it": [--y]) 
+	   and "on the line" - a [diagonal] pixel (same horizontal and vertical curve, but not necessarily on the line; moved by a circle line: [++x, --y]).
+
+	2. We will draw the pixel, to which the distance from the center of the circle is the minimum of 3 distances
+	   
+	   The distance to the pixel is determined as x^2 + y^2. If distance > R^2 the pixel is outside,
+	   distance < R^2 - inside, distance = R^2 - on the circle. Than our decision parameter, 
+	   let's call it [p = x^2 + y^2 - R^2], for example, if [p > 0 => pixel is outside]
+
+	3. The 3 pixels can be defined as: 
+	   outside (have to decrement y to draw a pixel closer to the circle)
+	   [outside] (++x) = [(x+1)^2 + y^2 - R^2]
+	   
+	   [diagonal] (x+1 and y-1) = [(x+1)^2 + (y-1)^2 - R^2]
+		
+	   [inside] (--y) = [x^2 + (y-1)^2 - R^2]
+
+	2. Let's determine which pixel to draw based on the distance to the diagonal pixel
+	   
+	   The diagonal pixel can either be outside the circle or inside.
+
+	   The decision parameter for the pixel is pDiag = (x+1)^2 + y^2 - R^2.
+
+	   1) If [pDiag > 0], the pixel is outside, we should draw either [diagonal or outside pixel]
+	   2) If [pDiag < 0], the pixel is inside, we should draw either [diagonal or inside pixel]
+
+	3. Determine which pixel to draw in each individual case (define decision parameters). To do that, find the shortest distance:
+
+	   pOutside, pInside and pDiag are distances. pInside < pDiag < pOutside
+
+	   1) Decision parameter pDiag > 0 => p1 = pOutside - pDiag = (x+1)^2 + y^2 - R^2 - (x+1)^2 - (y-1)^2 + R^2 =
+	      = y^2 - y^2 + 2y - 1 = 2y - 1 => [p1 = 2y-1]
+
+		  If pOutside < pDiag (the distance of the outside pixel is shorter; draw this pixel) 
+		  => pOutside - pDiag < 0 => if [p1 < 0], then draw the [outside pixel]
+		                             if [p1 > 0]: [diagonal]
+
+	   2) Decision parameter pDiag < 0 => p2 = pDiag - pInside = (x+1)^2 + (y-1)^2 - R^2 - x^2 - (y-1)^2 + R^2 = 
+	      = x^2 + 2x + 1 - x^2 = 2x + 1 => [p2 = 2x+1]
+
+		  [p2 < 0]: [diagonal], [p2 > 0]: [inside]
+
+	4. We have a decision parameter pDiag. Find pDiag STEPS to determine how we should update pDiag_Initial:
+
+	   pDiag_Initial = p_Diag(x = 0, y = R) = 1 + (R-1)^2 - R^2 = 1 + R^2 - 2R + 1 - R^2 = -2R + 2 = 2(1-R)] =>
+	   [pDiag_Initial = 2(1-R)]
+	   
+	   pDiag_Step_H = pDiag_Next_H ( pDiag(x = x+1, change of an outside pixel, i.e. horizontal step) ) - pDiag =
+	   = (x+2)^2 + (y-1)^2 - R^2 - (x+1)^2 - (y-1)^2 + R^2 = 2x + 3
+	   [pDiag_Step_H = 2x+3]
+	
+	   pDiag_Step_V = pDiag_Next_V ( pDiag(y = y-1), change of an inside pixel, i.e. vertical step) - pDiag =
+	   = (x+1)^2 + (y-2)^2 - R^2 - (x+1)^2 - (y-1)^2 + R^2 = -2y + 3
+	   [pDiag_Step_V = -2y+3]
+
+	   pDiag_Step = pDiag_Next ( pDiag(x = x+1, y = y-1) ) - pDiag
+	   = (x+2)^2 - (y-2)^2 + R^2 - (x+1)^2 - (y-1)^2 + R^2 = 2(x - y^2 + 3y + R^2 - 1)
+	   [pDiag_Step = 2(x - y^2 + 3y + R^2 - 1)]
+
+	5. Final values that will be used in the algorithm:
+
+	   Initial x, y: x=0, y=R
+
+	   [pDiag_Initial = 2(1-R)]
+
+
+	   Decision parameters:
+
+	   pDiag = (x+1)^2 + y^2 - R^2
+
+	   [pDiag > 0]: [diagonal or outside pixel]
+	   [pDiag < 0]: [diagonal or inside pixel]
+
+	   [p1 = 2y-1]
+	   [p1 < 0: outside]
+	   [p1 > 0: diagonal]
+
+	   [p2 = 2x+1]
+	   [p2 < 0: diagonal]
+	   [p2 > 0: inside]
+
+
+	   For steps: p = p + pStep:
+	   [pDiag_Step_H = 2x+3]; ++x
+	   [pDiag_Step_V = -2y+3]; --y
+	   [pDiag_Step = 2(x - y^2 + 3y + R^2 - 1)]; ++x, --y
+
+*/
+
+int main(int argc, char* argv[]) {
+	if (argc < 3) {
+		return 1;
+	}
+
+	const char* input = argv[1];
+	const char* output = argv[2];
+
+	std::ifstream inFile(input);
+	if (!inFile) {
+		return 2;
+	}
+
+	try 
+	{
+		int width = 0;
+		int height = 0;
+
+		inFile >> width;
+		inFile >> height;
+
+		rgbImg img(width, height);
+		RGB white{ 255, 255, 255 };
+
+		int x0 = 0;
+		int y0 = 0;
+		int R = 0;
+
+		while (inFile.get() != EOF)
+		{
+			// Collect start positions of x,y and a radius
+			inFile >> x0;  
+			inFile >> y0;
+			inFile >> R;
+
+			int x = 0;
+			int y = R;
+
+			int p = 2*(1 - R);  // pInitial
+
+			// Draw a circle
+			while (x < -y)  // The octant is restricted by the line x = -y (consider the grid)
+			{
+				int pDiag = (x + 1) * (x + 1) + y * y - R * R;
+
+				int p1 = 2 * y - 1; // horizontal or diagonal (outside pixel)
+				int p2 = 2 * x + 1; // vertical or diagonal (inside pixel)
+
+				if (pDiag > 0)  // draw a diagonal or an outside pixel
+				{
+					if (p1 < 0)  // draw an outside/horizontal pixel 
+					{
+						++x;
+						int pDiag_Step_H = 2 * x + 3;
+						p += pDiag_Step_H;
+					}
+					else  // p1 >= 0: draw a diagonal pixel
+					{
+						++x;
+						--y;
+						int pDiag_Step = 2 * (x - y * y + 3 * y + R * R - 1);
+						p += pDiag_Step;
+					}
+				}
+				else  // pDiag < 0: draw a diagonal or an inside pixel
+				{
+					if (p2 > 0)  // draw an inside/vertical pixel
+					{
+						--y;
+						int pDiag_Step_V = -2*y + 3;
+						p += pDiag_Step_V;
+					}
+					else  // p2 =< 0:  draw a diagonal pixel
+					{
+						++x;
+						--y;
+						int pDiag_Step = 2 * (x - y * y + 3 * y + R * R - 1);
+						p += pDiag_Step;
+					}
+				}
+				
+				// Color in the pixel; offset x and y coordinate by x0, y0
+				img.get(x + x0, y + y0) = white;  // y = x
+
+				// Duplicate the algorithm for 1 octant on all 8 octants
+				img.get(y + y0, x + x0) = white;  // x = y
+				img.get(x + x0, -y + y0) = white;  // x = -y
+				img.get(y + y0, -x + x0) = white;  // y = -x
+				img.get(-y + y0, -x + x0) = white;  // -y = -x
+				img.get(-x + x0, -y + y0) = white;  // -x = -y
+				img.get(-x + x0, y + y0) = white;  // -x = y
+				img.get(-y + y0, x + x0) = white;  // -y = x
+			}
+
+			img.save(output);
+		}
+	}
+	catch (std::exception& e) {
+		std::cout << "Error. " << e.what() << std::endl;
+	}
+	catch (...) {
+		std::cout << "Unknown error. " << std::endl;
+	}
+	inFile.close();
+	return 0;
+}
